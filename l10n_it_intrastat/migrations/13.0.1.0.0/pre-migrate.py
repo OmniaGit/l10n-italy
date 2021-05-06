b5651e8173edeb5c6a7715131edaae8b55e1c08b
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2021-05-06 17:46:27 +0200

    Add missing file

---
+++ l10n_it_intrastat/migrations/13.0.1.0.0/pre-migrate.py
@@ -0,0 +1,9 @@
+#  Copyright 2021 Simone Rubino - Agile Business Group
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+def migrate(cr, installed_version):
+    # move fields
+    # account.move.intrastat
+    # account.move.intrastat_line_ids
+    pass
