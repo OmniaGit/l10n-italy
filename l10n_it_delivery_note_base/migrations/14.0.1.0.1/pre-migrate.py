991cbc5fb957b65830b30145dafcc9c857fbd0a1
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2024-06-24 18:03:15 +0200

    FIX: copy paste

--- l10n_it_delivery_note_base/migrations/14.0.1.0.1/pre-migrate.py
+++ l10n_it_delivery_note_base/migrations/14.0.1.0.1/pre-migrate.py
@@ -7,7 +7,6 @@ from openupgradelib import openupgrade
 def migrate(env, version):
     # delete stock.delivery.note.type model data due to change
     # into via code creation for every company
-    module, table, name
     openupgrade.delete_sql_constraint_safely(env,
                                              '10n_it_delivery_note_base',
                                              'stock_delivery_note_type',
