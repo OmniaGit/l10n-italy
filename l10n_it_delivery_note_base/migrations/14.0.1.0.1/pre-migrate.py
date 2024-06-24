7f5a18925bf4c2c1faac6967395c53a68004f6e2
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2024-06-24 18:08:12 +0200

    IMP: lift constraint

--- l10n_it_delivery_note_base/migrations/14.0.1.0.1/pre-migrate.py
+++ l10n_it_delivery_note_base/migrations/14.0.1.0.1/pre-migrate.py
@@ -7,10 +7,9 @@ from openupgradelib import openupgrade
 def migrate(env, version):
     # delete stock.delivery.note.type model data due to change
     # into via code creation for every company
-    openupgrade.delete_sql_constraint_safely(env,
-                                             '10n_it_delivery_note_base',
-                                             'stock_delivery_note_type',
-                                             'stock_delivery_note_type_sequence_id_fkey')
+    openupgrade.lift_constraints(env,
+                                 'stock_delivery_note_type',
+                                 'sequence_id')
     #
     deleted_dn_type_data = (
         "l10n_it_delivery_note_base.delivery_note_type_incoming_ddt",
